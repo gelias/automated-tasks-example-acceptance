8334fd4d84fa5f7cae9ff8ba5ca7e98e68cc1ae3
Guilherme Elias <guilherme.elias@gmail.com>
2014-09-25 13:48:26 -0300

    Corrigida versao da app

--- spec/qconrio_spec.rb
+++ spec/qconrio_spec.rb
@@ -6,7 +6,7 @@ describe "QConRio" do
   it "Check Version" do
   	browser = Watir::Browser.new :firefox
 	browser.goto 'http://localhost:8080/automated-tasks-example/'
-	browser.h3.text.should eq("Version: 0.2")
+	browser.h3.text.should eq("Version: 0.3")
 	browser.close
   end
 end
